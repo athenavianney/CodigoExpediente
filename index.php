d1692ecbabbbfcae49c8392e590ffa3116f16f76
Athena <avnm96@gmail.com>
2016-04-29 00:55:16 -0700

    registrar arreglado, iconos modificados

--- index.php
+++ index.php
@@ -20,9 +20,7 @@
 	<div>
 		<h3>Programa expediente ortopedico</h3>
 	</div>
-
 	<body>
-
 		<!-- Wrapper-->
 			<div id="wrapper">
 
@@ -32,12 +30,10 @@
 						<a href="#registro" class="icon fa-pencil-square-o"><span>Registros</span></a>
 						<a href="#expediente" class="icon fa-folder"><span>Expedientes</span></a>
 						<a href="logout.php" class="icon fa-sign-out"><span>Salir</span></a>
-
 					</nav>
 
 				<!-- Main -->
 					<div id="main">
-
 						<!-- Pagina Principal -->
 							<article id="me" class="panel">
 								<header>
@@ -46,48 +42,11 @@
 									<p><br>Torre Médica CIMA</p>
 								</header>
 								<a class="jumplink pic">
-
 									<img src="images/pqq.jpg" alt="" />
 								</a>
 							</article>
-
-						<!-- Expediente -->
-							<article id="expediente" class="panel">
-								<header>
-										<form  method="post">
-										<!--<div class="row" > -->
-
-											 <div class="6u 12u$(mobile)">
-												<input type="text" name="busqueda" placeholder="Búsqueda" />
-											</div> 
-                                            <div class="table" >
-											<?php
-													$sql = "SELECT nombre, id FROM info_paciente";
-													$result = $conexion->query($sql);
-
-													if ($result->num_rows > 0) {
-													// output data of each row
-                                                        
-														echo " <table> <tr> <th> Nombre </th> </tr>";
-													   while($row = $result->fetch_assoc()) {
-                                                           $idp = $row["id"];
-														   echo "<tr> <td>" . $row["nombre"] ." </td> <td>". "<a href='paciente.php?id=".$idp." ' class='icon fa-eye active'></a> " . "</td> </tr>";
-													   }
-													} else {
-														echo "0 results";
-													}
-													$conexion->close();
-											?>
-											</div>
-                                            </div>
-										</div>
-								</form>
-								</header>
-								</p>
-
-							</article>
-
-						<!-- Registro -->
+                            
+                            <!-- Registro -->
 							<article id="registro" class="panel">
 								<header>
 									<h2>Registro de pacientes</h2>
@@ -104,20 +63,47 @@
 											<div class="6u$ 12u$(mobile)">
 												Fecha de nacimiento
 												<input type="date" name="fecha" placeholder="Fecha" required>
-
 											</div>
 											<div class="12u$">
 												<textarea name="antecedentes" placeholder="Antecedentes" rows="8" required></textarea>
 											</div>
 											<div class="12u$">
 												<input type="submit" name="registro" value="Registrar paciente">
-
 											</div>
 										</div>
 									</div>
 								</form>
 							</article>
 
+						<!-- Expediente -->
+							<article id="expediente" class="panel">
+								<header>
+									<form  method="post">
+                                    <!--<div class="row" > -->
+                                        <div class="6u 12u$(mobile)">
+                                            <input type="text" name="busqueda" placeholder="Búsqueda" />
+                                        </div> 
+                                        <div class="table" >
+                                            <?php
+                                                $sql = "SELECT nombre, id FROM info_paciente";
+                                                $result = $conexion->query($sql);
+
+                                                if ($result->num_rows > 0) {    
+                                                    echo " <table> <tr> <th> Nombre </th> </tr>";
+                                                    while($row = $result->fetch_assoc()) {
+                                                        $idp = $row["id"];
+                                                        echo "<tr> <td>" . $row["nombre"] ." </td> <td>". "<a href='paciente.php?id=".$idp." ' class='icon fa-eye active'></a> " ." </td> <td>". "<a href='paciente.php?id=".$idp." ' class='icon fa-pencil-square active'></a> " . "</td> </tr>";
+                                                    }
+                                                } else {
+                                                    echo "0 results";
+                                                }
+                                                $conexion->close();
+                                        ?>
+                                        </div>
+                                    </form>
+                                </header>
+                            </p>
+                        </article>						
 					</div>
 
 				<!-- Footer -->
@@ -126,14 +112,12 @@
 							<li>&copy; Untitled.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
 						</ul>
 					</div>
-			</div>
-
+			    </div>
 		<!-- Scripts -->
 			<script src="assets/js/jquery.min.js"></script>
 			<script src="assets/js/skel.min.js"></script>
 			<script src="assets/js/skel-viewport.min.js"></script>
 			<script src="assets/js/util.js"></script>
 			<script src="assets/js/main.js"></script>
-
 	</body>
 </html>
